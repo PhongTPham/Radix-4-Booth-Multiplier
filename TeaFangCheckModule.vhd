47e0e11108325530c4c126535503cd576723d7d1
ChrisAnnTerra <45477232+ChrisAnnTerra@users.noreply.github.com>
2020-03-11 00:04:29 -0700

    Update TeaFangCheckModule.vhd
    New updates to make it radix 4

--- TeaFangCheckModule.vhd
+++ TeaFangCheckModule.vhd
@@ -1,22 +1,18 @@
-
--- Company: Computer Architecture
--- Engineer: Phong Pham
--- Create Date: 3/7/2020 
--- Design Name: 
--- Module Name: AddModule - Behavioral 
--- Project Name: TeaFang Booth Radix-4
--- Target Devices: 
--- Tool versions: 
--- Description: 
---------------------------------
-
+--Company: Computer Architecture
+--Engineer: Christian Terrado & Phong Pham
+--
+--Create Date: 14:11:21 02/26/2016
+--Design Name:
+--Module Name: CheckModule - Behavioral
+--Project Name: Booth's Radix-4 Hardware
+-- Description: Checks last two bits in the accumlator register and outputs a control signal that follows booth's
+--		algorithm
 libraray IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity CheckModule is
-	port(Racc: IN std_logic_vector(30 downto 0); 
-	Rselect: OUT std_logic_vector(1 downto 0));
-Add ()1 + B, 10 -B, 00 0)
+    port(Racc: IN std_logic_vector(30 downto 0); 
+    Rselect: OUT std_logic_vector(2 downto 0)); -- Add ()
 end CheckModule
 
 architecture Behavioral of CheckModule is
@@ -25,15 +21,13 @@ begin
 
 process(Racc)
 begin
-	case Racc(2 downto 0) is
-		when "001" => Rselect <= "001"; --Add B
-		when "010" => Rselect <= "010"; --Add B
-		when "011" => Rselect <= "011"; --Add B
-		when "100" => Rselect <= "100"; --Add B
-		when "101" => Rselect <= "101"; --Add B
-		when "110" => Rselect <= "110"; --Add -B
-		when others => Rselect <= "000"; --Add 0
-	end case;
+    case Racc(2 downto 0) is
+        when "001" => Rselect <= "001"; --Add -2B
+        when "010" => Rselect <= "010"; --Add 2B
+        when "011" => Rselect <= "011"; -- Add -B
+        when "100" => Rselect <= "100"; -- Add B
+        when "others" => Rselect <= "000"; --Add 0
+    end case;
 end process;
 
 end Behavioral;
