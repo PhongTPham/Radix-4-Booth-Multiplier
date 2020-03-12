45746a21226953a04af2c663eac70eaa82b32cc6
PhongTPham <49416883+PhongTPham@users.noreply.github.com>
2020-03-11 21:58:05 -0700

    Update DoubleModule.vhd

--- DoubleModule.vhd
+++ DoubleModule.vhd
@@ -5,14 +5,14 @@
 -- Module Name: DoubleModule - Behavioral 
 -- Project Name: Booths Hardware 
 -- Description: Doubles the value of B
-
+----------------------------------------------------
 library IEEE; 
 use IEEE.STD_LOGIC_1164.ALL; 
 use ieee.numeric_std.all; 
 
 entity DoubleModule is 
     port(
-    Bplus: IN std_logic_vector(30 downto 0); - B value
+    Bplus: IN std_logic_vector(30 downto 0); -- B value
     DBout: OUT std_logic_vector(30 downto 0)); -- Output
     
 end DoubleModule; 
